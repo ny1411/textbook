e146592f690b96fceb5488a7d231a723d3d8c217
NYC! <neerajyamaji@gmail.com>
2026-08-17 00:42:23 +0530

    feat: implement model caching for embedding model using LRU cache

--- textbook-backend/services/embedder.py
+++ textbook-backend/services/embedder.py
@@ -1,5 +1,7 @@
+from functools import lru_cache
 from langchain_huggingface import HuggingFaceEmbeddings
 
+@lru_cache(maxsize=1)
 def bge_large_embedder():
     embeddings = HuggingFaceEmbeddings(
         model_name="BAAI/bge-large-en-v1.5",
