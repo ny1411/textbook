9c3f04ea248a1d5e9ccacf12f0e218d76b55d63d
NYC! <neerajyamaji@gmail.com>
2026-09-13 14:36:06 +0530

    refactor(config): add proxy rewrites for backend APIs

--- textbook-frontend/next.config.ts
+++ textbook-frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const BACKEND_URL = process.env.BACKEND_URL || "https://localhost:8080";
+
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${BACKEND_URL}/api/:path*`,
+      }
+    ]
+  },
 };
 
 export default nextConfig;
